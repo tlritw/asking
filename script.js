51c51ce13e5f5fe685d6d635da2c8dfb8fe5f8d5
tlritw <onlywow2013@gmail.com>
2024-01-16 17:34:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("Você aceitou sair comigo!")
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("opa, clickou errado...");
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%"
+}
